a04e7c60763f5ffe808d686f5d267163d6245578
glowredman <fredcraft00@gmail.com>
2022-02-14 17:36:52 +0100

    Update buildscript

--- src/main/java/lib/enderwizards/sandstone/Sandstone.java
+++ src/main/java/lib/enderwizards/sandstone/Sandstone.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.util.Map;
 
 // I'll probably end up using the Mod aspect of this for something, eventually.
-@Mod(modid = "libsandstone", name = "libsandstone", version = "1.0.0")
+@Mod(modid = "libsandstone", name = "libsandstone", version = "GRADLETOKEN_VERSION")
 public class Sandstone {
 
     private static Map<String, List<ModIntegration>> modIntegrations = new HashMap<String, List<ModIntegration>>();
